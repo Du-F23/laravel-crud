d53c66cacc535da769402e202496721f0efdb55e
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-01-28 22:25:06 -0600

    Modified aspects basic function

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('rol',['Administrador', 'Cliente']);
-            $table->rememberToken();
+            // $table->enum('rol',['Administrador', 'Cliente']);
+            // $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
